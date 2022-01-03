18dd2552e87bb007120c61d70d6856bba7328c98
Hamza Umar <humar6078@gmail.com>
2022-01-04 06:19:00 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-rn';
+
+export default function App() {
+  return (
+    <View style={tw("flex-1 justify-center items-center")}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+
